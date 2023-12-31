4072293b800d33d7f76210639f3d2406d2aad77a
Debasish Bhola <devop69@gmail.com>
2023-12-31 19:37:02 +0530

    Short Method added

--- Assignment3/A3_q1.sh
+++ Assignment3/A3_q1.sh
@@ -6,3 +6,9 @@ read num2
 sum=$((num1 + num2))
 echo "$sum"
 sum=$(expr $num1 + $num2)
+
+#short method
+#! /bin/bash
+# echo "Enter two numbers:"
+# read num1 num2
+# echo -e "Addition: $((num1+num2))\nSubtraction: $((num1-num2))\nMultiplication: $((num1*num2))\nDivision: $((num1/num2))"
