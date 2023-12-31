3cd4260e652d2de9119d1ce560dfe5314fcfb19e
Debasish Bhola <devop69@gmail.com>
2023-12-31 20:12:12 +0530

    Short Method added to A3q6.sh

--- Assignment3/A3q6.sh
+++ Assignment3/A3q6.sh
@@ -11,3 +11,10 @@ then
 else
 	echo "$yy is not a leap year."
 fi
+# echo "Enter the value of the year:"
+# read yy
+# if [[ ($((yy%4)) -eq 0) && ($((yy%100)) -ne 0) || ($((yy%400)) -eq 0) ]] then
+#   echo "Leap year"
+# else
+#   echo "Not leap year"
+# fi
