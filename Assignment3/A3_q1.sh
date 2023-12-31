b67073df58ee987ccacdf607afba46dc227dadcc
Debasish Bhola <devop69@gmail.com>
2023-12-31 20:21:55 +0530

    Moveed File

--- Assignment3/A3_q1.sh
+++ Assignment3/A3_q1.sh
@@ -1,14 +1,4 @@
-echo "Enter the value of the first no.:"
-read num1
-echo "Enter the value of the second no.:"
-read num2
-
-sum=$((num1 + num2))
-echo "$sum"
-sum=$(expr $num1 + $num2)
-
 #short method
-#! /bin/bash
-# echo "Enter two numbers:"
-# read num1 num2
-# echo -e "Addition: $((num1+num2))\nSubtraction: $((num1-num2))\nMultiplication: $((num1*num2))\nDivision: $((num1/num2))"
+echo "Enter two numbers:"
+read num1 num2
+echo -e "Addition: $((num1+num2))\nSubtraction: $((num1-num2))\nMultiplication: $((num1*num2))\nDivision: $((num1/num2))"
